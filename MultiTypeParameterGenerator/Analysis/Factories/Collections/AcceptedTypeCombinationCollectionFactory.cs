3a5ae6126d00c615c7dd4eae000473934945683f
MarvinMuellerGER <113536472+MarvinMuellerGER@users.noreply.github.com>
2025-10-26 13:19:19 +0100

    refactor AcceptedTypeCombinationCollectionFactory

--- MultiTypeParameterGenerator/Analysis/Factories/Collections/AcceptedTypeCombinationCollectionFactory.cs
+++ MultiTypeParameterGenerator/Analysis/Factories/Collections/AcceptedTypeCombinationCollectionFactory.cs
@@ -8,74 +8,159 @@ using MultiTypeParameterGenerator.Common.Models.TypedValues;
 namespace MultiTypeParameterGenerator.Analysis.Factories.Collections;
 
 using Key = (GenericType AffectedGenericType, int OtherAcceptedTypesHashCode);
+using TypeCombinationItem = (GenericType GenericType, AcceptedType AcceptedType);
 
 internal sealed class AcceptedTypeCombinationCollectionFactory : IAcceptedTypeCombinationCollectionFactory
 {
     public AcceptedTypeCombinationCollection Create(MethodToOverload methodToOverload)
     {
+        var typeCombinations = GetTypeCombinations(methodToOverload);
         var countByShortNameOfAcceptedTypeByAffectedGenericType = new Dictionary<Key, Dictionary<SourceCode, int>>();
         var countByAllUseTypeConstraints = 0;
 
-        return new(methodToOverload.AffectedGenericTypes.Values.ToDictionary(
-                affectedGenericType => affectedGenericType.AffectedGenericType,
-                affectedGenericType => affectedGenericType.AcceptedTypes.Values).GetCombinations()
-            .Select(acceptedTypeList =>
-            {
-                var countByGenericAcceptedType = new Dictionary<SourceCode, int>();
+        var combinations = typeCombinations
+            .Select(combination => CreateAcceptedTypeCombination(
+                combination,
+                countByShortNameOfAcceptedTypeByAffectedGenericType))
+            .SelectToReadonlyList(combination => TrackAllTypeConstraintsCombinations(
+                combination,
+                ref countByAllUseTypeConstraints));
 
-                return new AcceptedTypeCombination(
-                    acceptedTypeList.SelectToReadonlyList(item =>
-                    {
-                        var affectedGenericType = item.Item1;
-                        var acceptedType = item.Item2;
-
-                        var key = GetKey(acceptedTypeList, item);
-                        var countByShortNameOfAcceptedType =
-                            countByShortNameOfAcceptedTypeByAffectedGenericType.GetValueOrDefault(
-                                key, out var isNotFirstGenericAcceptedType, new())!;
-
-                        var shortAcceptedTypeName = acceptedType.Type.ShortenedSourceCodeWithoutContainingType;
-                        if (acceptedType.UseTypeConstraint)
-                        {
-                            countByShortNameOfAcceptedType[shortAcceptedTypeName] =
-                                countByShortNameOfAcceptedType.GetValueOrDefault(shortAcceptedTypeName) + 1;
-
-                            countByShortNameOfAcceptedTypeByAffectedGenericType[key] = countByShortNameOfAcceptedType;
-                            
-                            countByGenericAcceptedType[shortAcceptedTypeName] =
-                                countByGenericAcceptedType.GetValueOrDefault(shortAcceptedTypeName) + 1;
-                        }
-
-                        return new AcceptedTypeForAffectedGenericType(
-                            affectedGenericType,
-                            acceptedType with
-                            {
-                                IndexOfParametersWithSameType =
-                                countByGenericAcceptedType.GetValueOrDefault(shortAcceptedTypeName),
-
-                                IndexOfParametersWithSameShortTypeName =
-                                countByShortNameOfAcceptedType.Count > 1 ||
-                                acceptedTypeList.Count(a => a.Item2.UseTypeConstraint) > 1
-                                    ? countByShortNameOfAcceptedType.GetValueOrDefault(shortAcceptedTypeName)
-                                    : 0
-                            }, isNotFirstGenericAcceptedType);
-                    }));
-            }).SelectToReadonlyList(acceptedTypeCombination =>
-            {
-                if (!acceptedTypeCombination.AllUseTypeConstraints)
-                    return acceptedTypeCombination;
-
-                countByAllUseTypeConstraints++;
-                return acceptedTypeCombination with
-                {
-                    IndexOfCombinationsWhereAllUseTypeConstraints = countByAllUseTypeConstraints
-                };
-            }));
+        return new(combinations);
     }
 
-    private static Key GetKey(
-        IReadOnlyList<(GenericType, AcceptedType)> acceptedTypeList, (GenericType, AcceptedType) item) =>
-        (item.Item1, acceptedTypeList.Except([item])
-            .Select(otherAcceptedType => otherAcceptedType.Item2.Type.ShortenedSourceCodeExclNullableAnnotation.Value)
-            .Join().GetHashCode());
+    private static IEnumerable<IReadOnlyList<TypeCombinationItem>> GetTypeCombinations(
+        MethodToOverload methodToOverload)
+    {
+        var acceptedTypesByGenericType = methodToOverload.AffectedGenericTypes.Values.ToDictionary(
+            affectedGenericType => affectedGenericType.AffectedGenericType,
+            affectedGenericType => affectedGenericType.AcceptedTypes.Values);
+
+        return acceptedTypesByGenericType.GetCombinations();
+    }
+
+    private static AcceptedTypeCombination CreateAcceptedTypeCombination(
+        IReadOnlyList<TypeCombinationItem> acceptedTypeList,
+        Dictionary<Key, Dictionary<SourceCode, int>> countByShortNameOfAcceptedTypeByAffectedGenericType)
+    {
+        var countByGenericAcceptedType = new Dictionary<SourceCode, int>();
+
+        var acceptedTypesForGenericTypes = acceptedTypeList.SelectToReadonlyList(item =>
+            CreateAcceptedTypeForAffectedGenericType(
+                item,
+                acceptedTypeList,
+                countByShortNameOfAcceptedTypeByAffectedGenericType,
+                countByGenericAcceptedType));
+
+        return new(acceptedTypesForGenericTypes);
+    }
+
+    private static AcceptedTypeForAffectedGenericType CreateAcceptedTypeForAffectedGenericType(
+        TypeCombinationItem item,
+        IReadOnlyList<TypeCombinationItem> acceptedTypeList,
+        Dictionary<Key, Dictionary<SourceCode, int>> countByShortNameOfAcceptedTypeByAffectedGenericType,
+        Dictionary<SourceCode, int> countByGenericAcceptedType)
+    {
+        var (genericType, acceptedType) = item;
+        var key = CreateContextKey(acceptedTypeList, item);
+
+        var countByShortNameOfAcceptedType = countByShortNameOfAcceptedTypeByAffectedGenericType
+            .GetValueOrDefault(key, out var isNotFirstOccurrence, new())!;
+
+        var shortAcceptedTypeName = acceptedType.Type.ShortenedSourceCodeWithoutContainingType;
+
+        if (acceptedType.UseTypeConstraint)
+        {
+            UpdateTypeCounts(
+                countByShortNameOfAcceptedType,
+                countByGenericAcceptedType,
+                shortAcceptedTypeName);
+
+            countByShortNameOfAcceptedTypeByAffectedGenericType[key] = countByShortNameOfAcceptedType;
+        }
+
+        var updatedAcceptedType = UpdateAcceptedTypeIndices(
+            acceptedType,
+            shortAcceptedTypeName,
+            acceptedTypeList,
+            countByShortNameOfAcceptedType,
+            countByGenericAcceptedType);
+
+        return new(
+            genericType,
+            updatedAcceptedType,
+            isNotFirstOccurrence);
+    }
+
+    private static void UpdateTypeCounts(
+        Dictionary<SourceCode, int> countByShortNameOfAcceptedType,
+        Dictionary<SourceCode, int> countByGenericAcceptedType,
+        SourceCode shortAcceptedTypeName)
+    {
+        countByShortNameOfAcceptedType[shortAcceptedTypeName] =
+            countByShortNameOfAcceptedType.GetValueOrDefault(shortAcceptedTypeName) + 1;
+
+        countByGenericAcceptedType[shortAcceptedTypeName] =
+            countByGenericAcceptedType.GetValueOrDefault(shortAcceptedTypeName) + 1;
+    }
+
+    private static AcceptedType UpdateAcceptedTypeIndices(
+        AcceptedType acceptedType,
+        SourceCode shortAcceptedTypeName,
+        IReadOnlyList<TypeCombinationItem> acceptedTypeList,
+        Dictionary<SourceCode, int> countByShortNameOfAcceptedType,
+        Dictionary<SourceCode, int> countByGenericAcceptedType)
+    {
+        var indexOfParametersWithSameType = countByGenericAcceptedType.GetValueOrDefault(shortAcceptedTypeName);
+        var indexOfParametersWithSameShortTypeName = CalculateShortTypeNameIndex(
+            acceptedTypeList,
+            countByShortNameOfAcceptedType,
+            shortAcceptedTypeName);
+
+        return acceptedType with
+        {
+            IndexOfParametersWithSameType = indexOfParametersWithSameType,
+            IndexOfParametersWithSameShortTypeName = indexOfParametersWithSameShortTypeName
+        };
+    }
+
+    private static int CalculateShortTypeNameIndex(
+        IReadOnlyList<TypeCombinationItem> acceptedTypeList,
+        Dictionary<SourceCode, int> countByShortNameOfAcceptedType,
+        SourceCode shortAcceptedTypeName)
+    {
+        var hasMultipleTypesWithSameName = countByShortNameOfAcceptedType.Count > 1;
+        var hasMultipleTypeConstraints = acceptedTypeList.Count(item => item.AcceptedType.UseTypeConstraint) > 1;
+
+        return hasMultipleTypesWithSameName || hasMultipleTypeConstraints
+            ? countByShortNameOfAcceptedType.GetValueOrDefault(shortAcceptedTypeName)
+            : 0;
+    }
+
+    private static AcceptedTypeCombination TrackAllTypeConstraintsCombinations(
+        AcceptedTypeCombination acceptedTypeCombination,
+        ref int countByAllUseTypeConstraints)
+    {
+        if (!acceptedTypeCombination.AllUseTypeConstraints)
+            return acceptedTypeCombination;
+
+        countByAllUseTypeConstraints++;
+        return acceptedTypeCombination with
+        {
+            IndexOfCombinationsWhereAllUseTypeConstraints = countByAllUseTypeConstraints
+        };
+    }
+
+    private static Key CreateContextKey(
+        IReadOnlyList<TypeCombinationItem> acceptedTypeList,
+        TypeCombinationItem item)
+    {
+        var otherAcceptedTypesHashCode = acceptedTypeList
+            .Except([item])
+            .Select(otherItem => otherItem.AcceptedType.Type.ShortenedSourceCodeExclNullableAnnotation.Value)
+            .Join()
+            .GetHashCode();
+
+        return (item.GenericType, otherAcceptedTypesHashCode);
+    }
 }
